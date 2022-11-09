19053939fc991770807b571634fb1ff110c12a57
Rafael Luna <Rafaellluna@hotmail.com>
2022-11-08 22:57:48 -0300

    att

--- Assets/MainMenu/scripts/LoginMenuController.cs
+++ Assets/MainMenu/scripts/LoginMenuController.cs
@@ -11,6 +11,7 @@ public class LoginMenuController : MonoBehaviour {
     public Button LoginButton;
 
     public GameObject CoursesScreen;
+    public GameObject ClassesScreen;
 
     /// <summary>Obtém o usuário digitado.</summary>
     private string Login => this.LoginField.text.Trim();
@@ -30,12 +31,43 @@ public class LoginMenuController : MonoBehaviour {
     }
 
     /// <summary>Realiza o login do usuário.</summary>
-    public void DoLogin()
+    public void GoCourse()
     {
         Debug.Log("Usuário: " + this.Login + " | Senha: " + this.Password);
 
         // Esconder tela de login e exibir tela de cursos.
         this.LoginScreen.SetActive(false);
         this.CoursesScreen.SetActive(true);
+    }
+
+    public void GoClasses()
+    {
+        this.CoursesScreen.SetActive(false);
+        this.ClassesScreen.SetActive(true);
+    }
+
+    public void GoVideo()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("classroom");
+    }
+
+    public void BackScreen()
+    {
+        if (this.CoursesScreen.activeSelf == true)
+        {
+            this.LoginScreen.SetActive(true);
+            this.CoursesScreen.SetActive(false);
+        }
+        
+        if (this.ClassesScreen.activeSelf == true)
+        {
+            this.CoursesScreen.SetActive(true);
+            this.ClassesScreen.SetActive(false);
+        } 
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
     }
 }
